42c2276edae601055defae64de78022cd7265cc6
Emily Oriel <58639088+emilyoriel@users.noreply.github.com>
2020-05-12 16:31:21 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function getUserNumberInput () {
+    return parseInt(userInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+//still seem like way too many lines of code for such  a simplified thing!!!
+/*def gotta improve it!!!!*/
+
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const inititalResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+createAndWriteOutput('+', inititalResult, enteredNumber)
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const inititalResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', inititalResult, enteredNumber);
+}
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const inititalResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', inititalResult, enteredNumber);
+}
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const inititalResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', inititalResult, enteredNumber);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
